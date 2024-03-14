52afd0da6cd537988d52cc06fd74b44e0f19ee89
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-03-14 18:52:29 +0300

    feat: add slidev-addon-qrcode

---
+++ components/QRCode.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="qr-code" ref="qrCodeRef"></div>
+</template>
+
+<script setup lang="ts">
+import QRCodeStyling, { type Options, } from 'qr-code-styling';
+
+const props = withDefaults(defineProps<Options>(), {
+    type: "canvas",
+    width: 300,
+    height: 300,
+    data: "",
+    margin: 0,
+    qrOptions: () => ({
+        typeNumber: 0,
+        mode: undefined,
+        errorCorrectionLevel: "Q"
+    }),
+    imageOptions: () => ({
+        hideBackgroundDots: true,
+        imageSize: 0.4,
+        crossOrigin: undefined,
+        margin: 0
+    }),
+    dotsOptions: () => ({
+        type: "square",
+        color: "#000"
+    }),
+    backgroundOptions: () => ({
+        color: "#fff"
+    })
+});
+
+const qrCode = new QRCodeStyling(props);
+
+import { onMounted, ref } from 'vue';
+
+const qrCodeRef = ref(null);
+onMounted(() => {
+    if (qrCodeRef.value) qrCode.append(qrCodeRef.value);
+});
+</script>
---
+++ example.md
@@ -0,0 +1,11 @@
+<QRCode
+:width="300"
+:height="300"
+type="svg"
+data="https://sli.dev"
+:margin="10"
+:imageOptions="{ margin: 10 }"
+:backgroundOptions="{ color: 'black' }"
+:dotsOptions="{ type: 'extra-rounded', color: 'purple' }"
+image="/logo.svg"
+/>
